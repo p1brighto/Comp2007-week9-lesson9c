10452e4e30af08fdfa6f4b50ddd059a67c9ae09a
p1brighto <p1brighto@gmail.com>
2016-07-14 17:42:06 -0400

    Add project files.

---
+++ Comp2007-week9-lesson9c/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Comp2007_week9_lesson9c
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Comp2007-week9-lesson9c/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2007_week9_lesson9c.Startup))]
+namespace Comp2007_week9_lesson9c
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
